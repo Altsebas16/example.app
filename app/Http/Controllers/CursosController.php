7e09a796f018183f0fcdb37eda3b17220837e1a1
Altsebas16 <johan.sebastian.4826@gmail.com>
2024-09-30 08:38:17 -0500

    first commit

---
+++ app/Http/Controllers/CursosController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CursosController extends Controller
+{
+
+public function index(){
+    //return "Bienvenidos a la pagina principal de los cursos desde el controlador";
+    return view('cursos.index');
+    }
+
+public function create(){
+    return view('cursos.create');
+    }
+public function show($curso){
+   //return "curso $curso";
+    return view('cursos.show', ['curso'=> $curso]);
+}
+
+}
+
+
